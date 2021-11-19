d5ab767344c2c7fc34e2527379431bcf78d44924
WilsonPlur <yourshows12@gmail.com>
2021-11-19 18:19:15 +0200

    typescript-simple-blog

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8c0016ccf53623be104b5d471a565a31'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c51ec518-916f-4389-a950-04827d74808d'
+};
